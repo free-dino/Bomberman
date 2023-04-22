bc5b4521143bdbc5f2d19f974b703693a88932e8
Trần An Thắng <anthang2003.nd@gmail.com>
2023-04-22 18:42:33 +0700

    Constructor for 2 basic enemy

---
+++ bomberman/src/entities/character/enemy/Balloom.java
@@ -0,0 +1,14 @@
+package entities.character.enemy;
+
+import javafx.scene.image.Image;
+
+public class Balloom extends Enemy {
+    public Balloom(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
---
+++ bomberman/src/entities/character/enemy/Enemy.java
@@ -0,0 +1,12 @@
+package entities.character.enemy;
+
+import entities.character.Animal;
+import javafx.scene.image.Image;
+
+public abstract class Enemy extends Animal {
+    public Enemy(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+
+}
