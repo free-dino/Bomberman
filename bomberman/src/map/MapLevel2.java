3b4bdf574d7be372db7ef00e621746afbac1e2a6
Trần An Thắng <anthang2003.nd@gmail.com>
2023-04-25 00:33:48 +0700

    Nghịch ngu

--- bomberman/src/map/MapLevel2.java
+++
@@ -1,7 +0,0 @@
-package map;
-
-public class MapLevel2 {
-    public MapLevel2() {
-        new CreateMap("res/levels/Level2.txt");
-    }
-}
--- bomberman/src/map/MapLevel3.java
+++
@@ -1,7 +0,0 @@
-package map;
-
-public class MapLevel3 {
-    public MapLevel3() {
-        new CreateMap("res/levels/Level3.txt");
-    }
-}
